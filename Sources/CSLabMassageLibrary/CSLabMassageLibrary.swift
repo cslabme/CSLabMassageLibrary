54d6c1773760ddbac3f92c0a7d5872ecf627840d
yaman <komeda@peche.co.jp>
2024-02-15 12:34:19 +0900

    Create package

---
+++ Sources/CSLabMassageLibrary/CSLabMassageLibrary.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+extension String {
+
+    /// Transform Hiragana to Katakana.
+    /// - Returns: The transformed string.
+    /// - SeeAlso: ``toHiragana()``
+    public func toKatakana() -> String? {
+        return "HOGE"
+    }
+}
