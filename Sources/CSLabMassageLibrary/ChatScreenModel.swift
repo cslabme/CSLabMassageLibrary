5eddde43f8f1142e65020b97f5fa878fea06a7b6
yaman <komeda@peche.co.jp>
2024-02-16 10:04:44 +0900

    Create package3

--- Sources/CSLabMassageLibrary/ChatScreenModel.swift
+++ Sources/CSLabMassageLibrary/ChatScreenModel.swift
@@ -1,8 +1,33 @@
 //
-//  File.swift
-//  
+//  ChatScreenModel.swift
+//
 //
 //  Created by 米田大介 on 2024/02/16.
 //
 
 import Foundation
+
+final class ChatScreenModel: ObservableObject {
+    private var webSocketTask: URLSessionWebSocketTask?
+    
+    // WebSocketへの接続を行います
+    func connect() {
+        let channelId = "1"
+        let url = URL(string: "wss://op6tgkcltl.execute-api.ap-northeast-1.amazonaws.com/production/ -s abcdefghijklmnopqrstuvwxyg -s" + channelId)!
+        webSocketTask = URLSession.shared.webSocketTask(with: url)
+        // メッセージを受け取った時に呼ばれるハンドラ
+        webSocketTask?.receive(completionHandler: onReceive)
+        webSocketTask?.resume()
+    }
+
+    // 接続解除時に実行する関数
+    func disconnect() {
+        webSocketTask?.cancel(with: .normalClosure, reason: nil)
+    }
+    
+    // メッセージを受け取った時に呼ばれる関数
+    private func onReceive(incoming: Result<URLSessionWebSocketTask.Message, Error>) {
+        // 2回目以降の記事で解説
+    }
+
+}
--- Sources/CSLabMassageLibrary/CsLabMassageView.swift
+++ Sources/CSLabMassageLibrary/CsLabMassageView.swift
@@ -2,29 +2,155 @@
 //  CsLabMassageView.swift
 //
 //
-//  Created by 米田大介 on 2024/02/15.
+//  CsLabMassageView by 米田大介 on 2024/02/15.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct ChatMessage: Hashable {
+    let content: String
+    let type: String
+}
+
+// チャット表示用のメインビュー
+struct CsLabMassageView: View {
+
+    // 現在のチャットが完了しているかどうかを示す変数
+    @State private var isCompleting: Bool = false
+    
+    // ユーザーが入力するテキストを保存する変数
+    @State private var text: String = ""
+    
+    // チャットメッセージの配列
+    @State private var chat: [ChatMessage] = [
+        ChatMessage(content: "あなたは、ユーザーの質問や会話に回答するロボットです", type:"system")
+    ]
+    
+    // WSSコネクションを行うクラス
+    let wss = ChatScreenModel().connect()
+    
+    // チャット画面のビューレイアウト
     var body: some View {
-        List {
-            // リスト内に複数の要素を記述
-            Text("りんご")
-            Text("オレンジ")
-            Text("レモン")
-            Text("いちご")
-            Text("キウイ")
-            Text("りんご")
-            Text("オレンジ")
-            Text("レモン")
-            Text("いちご")
-            Text("キウイ")
+        VStack {
+            // スクロール可能なメッセージリストの表示
+            ScrollViewReader { reader in
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        ForEach(chat.indices, id: \.self) { index in
+                            MessageView(message: chat[index])
+                        }
+                    }
+                }
+            }
+            .padding(.top)
+            // 画面をタップしたときにキーボードを閉じる
+            .onTapGesture {
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
+            
+            // テキスト入力フィールドと送信ボタンの表示
+            HStack {
+                // テキスト入力フィールド
+                TextField("メッセージを入力", text: $text)
+                    .disabled(isCompleting) // チャットが完了するまで入力を無効化
+                    .font(.system(size: 15)) // フォントサイズを調整
+                    .padding(8)
+                    .padding(.horizontal, 10)
+                    .background(Color.white) // 入力フィールドの背景色を白に設定
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Color.gray.opacity(0.5), lineWidth: 1.5)
+
+                    )
+                
+                // 送信ボタン
+                Button(action: {
+                    isCompleting = true
+                    // ユーザーのメッセージをチャットに追加
+                    chat.append(ChatMessage(content: text, type:"user"))
+                    text = "" //テキストフィールドをクリア
+                    Task {
+                        do {
+                            // OpenAIの設定
+                        
+                                                
+                            // チャットの生成
+                            isCompleting = false
+                        } catch {
+                            print("ERROR DETAILS - \(error)")
+                        }
+                    }
+                }) {
+                    // 送信ボタンのデザイン
+                    Image(systemName: "arrow.up.circle.fill")
+                        .font(.system(size: 30))
+                        .foregroundColor(self.text == "" ? Color(#colorLiteral(red: 0.75, green: 0.95, blue: 0.8, alpha: 1)) : Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)))
+                }
+                // テキストが空またはチャットが完了していない場合はボタンを無効化
+                .disabled(self.text == "" || isCompleting)
+            }
+            
+            .padding(.horizontal)
+            .padding(.bottom, 8) // 下部のパディングを調整
+        }
+    }
+}
+
+// メッセージのビュー
+struct MessageView: View {
+    
+    var message: ChatMessage
+    
+    var body: some View {
+        HStack {
+            if message.type == "user" {
+                Spacer()
+            } else {
+                // ユーザーでない場合はアバターを表示
+                AvatarView(imageName: "avatar")
+                    .padding(.trailing, 8)
+            }
+            VStack(alignment: .leading, spacing: 4) {
+                // メッセージのテキストを表示
+                Text(message.content)
+                    .font(.system(size: 14)) // フォントサイズを調整
+                    .foregroundColor(message.type == "user" ? .white : .black)
+                    .padding(10)
+                    .id(999)
+                    // ユーザーとAIのメッセージで背景色を変更
+                    .background(message.type == "user" ? Color(#colorLiteral(red: 0.2078431373, green: 0.7647058824, blue: 0.3450980392, alpha: 1)) : Color(#colorLiteral(red: 0.9098039216, green: 0.9098039216, blue: 0.9176470588, alpha: 1)))
+                    .cornerRadius(20) // 角を丸くする
+            }
+            .padding(.vertical, 5)
+            // ユーザーのメッセージの場合は右側にスペースを追加
+            if message.type != "user" {
+                Spacer()
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+// アバタービュー
+struct AvatarView: View {
+    var imageName: String
+    
+    var body: some View {
+        VStack {
+            // アバター画像を円形に表示
+            Image(systemName: "person.crop.circle")
+                .resizable()
+                .frame(width: 30, height: 30)
+                .clipShape(Circle())
+            
+            // AIの名前を表示
+            Text("サポート")
+                .font(.caption) // フォントサイズを小さくするためのオプションです。
+                .foregroundColor(.black) // テキストの色を黒に設定します。
         }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    CsLabMassageView()
 }
