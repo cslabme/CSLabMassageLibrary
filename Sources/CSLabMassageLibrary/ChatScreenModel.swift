c63117c62a180e9a4c2e7b92cea8a1a3e5795575
yaman <komeda@peche.co.jp>
2024-02-16 10:00:44 +0900

    Create package2

---
+++ Sources/CSLabMassageLibrary/ChatScreenModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 米田大介 on 2024/02/16.
+//
+
+import Foundation
---
+++ Sources/CSLabMassageLibrary/CsLabMassageView.swift
@@ -0,0 +1,30 @@
+//
+//  CsLabMassageView.swift
+//
+//
+//  Created by 米田大介 on 2024/02/15.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        List {
+            // リスト内に複数の要素を記述
+            Text("りんご")
+            Text("オレンジ")
+            Text("レモン")
+            Text("いちご")
+            Text("キウイ")
+            Text("りんご")
+            Text("オレンジ")
+            Text("レモン")
+            Text("いちご")
+            Text("キウイ")
+        }
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
